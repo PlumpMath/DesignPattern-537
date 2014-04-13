5a3a0b8a1a15fb81d682d606a9ca19528cfeb26f
j-kubota <electrolite14@gmail.com>
2014-04-13 16:58:45 +0900

    update iterator

---
+++ src/singleton/Singleton.java
@@ -0,0 +1,11 @@
+package Singleton;
+
+public class Singleton {
+	private static final Singleton instance = new Singleton();
+	private Singleton(){		
+		
+	}
+	public static Singleton getInstance(){
+		return Singleton.instance;
+	}
+}
