775567ae32d9b867c6d57f361098ae3f47b9f97c
j-kubota <electrolite14@gmail.com>
2014-04-13 17:06:57 +0900

    iterator update

---
+++ src/iterator/Iterator.java
@@ -0,0 +1,10 @@
+package iterator;
+
+public interface Iterator {
+	public abstract boolean hasNext();
+	public abstract Object next();
+	public abstract boolean hasPrevious();
+	public abstract Object previous();
+	public abstract void decIterator();
+	public abstract void incIterator();
+}
